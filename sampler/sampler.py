90bdda948f947c4f54950fb78061c7e77f19ee31
fsazo <fsazo@uc.cl>
2024-05-08 13:11:36 -0400

    Tracer y Sampler

--- sampler/sampler.py
+++ sampler/sampler.py
@@ -4,13 +4,14 @@ from time import sleep
 import traceback
 from sys import _current_frames
 
-
 class Sampler:
     def __init__(self, tid) -> None:
         self.tid = tid
         self.t = threading.Thread(target=self.sample, args=())
         self.active = True
-        
+        self.counts_dict = {}
+        self.num_seconds = 0
+
     def start(self):
         self.active = True
         self.t.start()
@@ -27,13 +28,39 @@ class Sampler:
                     code = frame.f_code.co_name
                     stack.append(code)
                 stack.reverse()
-                print(stack)  # Esta linea imprime el stack despues de invertirlo la pueden comentar o descomentar si quieren
+                #print(stack)  # Esta linea imprime el stack despues de invertirlo la pueden comentar o descomentar si quieren
+                return stack 
     
     def sample(self):
         while self.active:
-            self.checkTrace()
+            stack = self.checkTrace()
+            if stack:
+                self.update_counts_dict(stack)
+                self.num_seconds += 1
             sleep(1)
 
+
+
+    def update_counts_dict(self, stack):
+        node = self.counts_dict #obtiene el diccionario actual.
+        for func in stack:  #le suma uno al elemento correspondiente del diccionario cada vez que aparece en
+        #el stack y lo agrega al diccionario en caso de que no esté (es decir, es la primera vez que se llama)        
+            if func not in node:
+                node[func] = {"count": 1}  
+            else:
+                node[func]["count"] += 1 
+            node = node[func] #genera que los diccionarios se guarden uno dentro de otro, para luego
+            #al imprimir generar la estructura del árbol.
+
+
     def print_report(self):
-        # Este metodo debe imprimir el reporte del call context tree
-        pass
+        print(f"total ({self.num_seconds} seconds)") #imprime la cantidad total de segundos.
+        self.print_tree(self.counts_dict)
+
+    #función recursiva que imprime el diccionario counts_dict en forma de árbol  
+    def print_tree(self, counts_dict, indent=1):   
+        for func, child_node in counts_dict.items(): #revisa la función y el nodo hijo (función que se ejecuta después de ella)
+            if isinstance(child_node, dict):  #revisa si el nodo hijo es un diccionario.
+                count = child_node.get("count", 0) #obtiene el número de veces que esa función en esa profundidad es llamada.
+                print("    " * indent + f"{func} ({count} seconds)")
+                self.print_tree(child_node, indent + 1)  #llamanda recursiva aumentando la indentación.
--- tracer/coverage_tracer.py
+++ tracer/coverage_tracer.py
@@ -4,6 +4,7 @@ import inspect
 from types import *
 import traceback
 from stack_inspector import StackInspector
+from line_record import LineRecord  
 
 """ Clase para la Tarea 2. Para su uso, considere:
 with CoverageTracer() as covTracer:
@@ -16,16 +17,40 @@ class CoverageTracer(StackInspector):
 
     def __init__(self):
         super().__init__(None, self.traceit)
-        # Completa el codigo necesario
+        self.executed_lines = [] #lista que almacena las líneas ejecutadas.
+        self.freq = {} #diccionario que almacena la cantidad de veces que cada línea fue ejecutada.
 
-    # Completa la funcion de rastreo
+
+    # esta función la copié de la función traceit del archivo function_tracer.py del código base
     def traceit(self, frame, event: str, arg):
-        pass
+        if event == 'line':
+            function_name = frame.f_code.co_name
+            line = frame.f_lineno
+            if not self.our_frame(frame) and not self.problematic_frame(frame):
+                self.executed_lines.append((function_name, line))
+    
+        return self.traceit
 
     def print_lines_report(self):
         print("{:<30} {:<10} {:<10}".format('fun', 'line', 'freq'))
-        # Completa el codigo necesario
+        #revisa si la función ya fue ejecutada por primera vez, en caso de no haber sido ejecutada se agrega al diccionario
+        #En caso contrario, se suma uno a la frecuencia de la función
+        for function_name, line in self.executed_lines:
+            if (function_name, line) in self.freq:
+                self.freq[(function_name, line)] += 1
+            else:
+                self.freq[(function_name, line)] = 1
+        #Ordena la lista según el número de línea.
+        sorted_list = sorted(self.freq.items(), key=lambda a: a[0][1]) # esta función la copié de la función report_executed_functions del archivo function_tracer.py del código base
+        #imprime lo pedido 
+        for (function_name, line), freq in sorted_list:
+            print("{:<30} {:<10} {:<10}".format(function_name, line, freq))
 
     def report_executed_lines(self):
-        self.print_lines_report()
-        # Completa el codigo necesario
+        self.print_lines_report() #imprime el output 
+        line_records_instances = [] #lista de instancias de la clase Line Record
+        sorted_list = sorted(self.freq.items(), key=lambda a: a[0][1]) #ordena la lista según el número de línea
+        for (function_name, line_number), freq in sorted_list:
+            line_records_instances.append(LineRecord.new_instance_with(function_name, line_number, freq)) #genera una instancia de 
+            #Line Record y la agrega a la lista.
+        return line_records_instances #devuelve la lista de instancias de la clase Line Record
